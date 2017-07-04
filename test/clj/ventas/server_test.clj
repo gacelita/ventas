0fbdd59cf22728e53d5509e9f44999d355aa2601
Joel Sánchez <webmaster@kazer.es>
2017-07-05 00:46:16 +0200

    Remove useless code

--- test/clj/ventas/server_test.clj
+++ test/clj/ventas/server_test.clj
@@ -3,27 +3,6 @@
             [ventas.server :as server]
             [ventas.database :as db]))
 
-(comment 
-
-
-    (defmethod ws-request-handler :users.list [message state]
-      (db/entity-query :user))
-
-    (defmethod ws-request-handler :users.made-comments.list [message state]
-      (db/entity-query :comment {:source (get-in message [:params :id])}))
-
-    (defmethod ws-request-handler :users.comments.list [message state]
-      (db/entity-query :comment {:target (get-in message [:params :id])}))
-
-    (defmethod ws-request-handler :users.friends.list [message state]
-        (concat (map :source (db/entity-query :friendship {:target (get-in message [:params :id])}))
-                (map :target (db/entity-query :friendship {:source (get-in message [:params :id])}))))
-
-    (defmethod ws-request-handler :users.save [message state]
-      (db/entity-upsert :user (:params message))))
-
-
-
 (deftest entities-remove
     (let [user (db/entity-create :user (db/generate-1 :schema.type/user))
           result (server/ws-request-handler {:name :entities.remove :params {:id (:id user)}} {})]
