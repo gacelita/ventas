2e64aa51eb1a75598be6a9809c742d3fce996449
Joel Sánchez <webmaster@kazer.es>
2017-07-09 21:28:35 +0200

    Product

--- src/cljs/ventas/themes/mariscosriasbajas/pages/product.cljs
+++ src/cljs/ventas/themes/mariscosriasbajas/pages/product.cljs
@@ -2,10 +2,12 @@
   (:require [reagent.core :as reagent :refer [atom]]
             [reagent.session :as session]
             [re-frame.core :as rf]
+            [fqcss.core :refer [wrap-reagent]]
             [bidi.bidi :as bidi]
             [re-frame-datatable.core :as dt]
             [ventas.page :refer [pages]]
-            [ventas.util :refer [route-param]]
+            [ventas.util :refer [route-param value-handler]]
+            [soda-ash.core :as sa]
             [ventas.themes.mariscosriasbajas.components.skeleton :refer [skeleton]]))
 
 (rf/reg-sub ::product
@@ -21,6 +23,25 @@
 (defmethod pages :frontend.product []
   (rf/dispatch [::product])
   [skeleton
-    [:div
-      [:h2 "Test frontend product" (route-param :id)]
-      [:pre (.stringify js/JSON (clj->js @(rf/subscribe [::product])) nil 2)]]])
+   (reagent/with-let [data (atom {:quantity 1})]
+     (wrap-reagent
+      [sa/Container
+       [:div {:fqcss [::product]}
+        (let [product @(rf/subscribe [::product])]
+          [:div
+           [:div {:fqcss [::top]}
+            [:div {:fqcss [::images]}
+             (for [image (:images product)]
+               [:img {:key (:id image) :src (:url image)}])]
+            [:div {:fqcss [::info]}
+             [:h1 {:fqcss [::name]} (:name product)]
+             [:h2 {:fqcss [::price]} (:price product)]
+             [:p {:fqcss [::description]} (:description product)]
+             [:div {:fqcss [::quantity]}
+              [:button {:type "button" :on-click #(swap! data update :quantity dec)} [sa/Icon {:name "minus"}]]
+              [:input {:type "text" :value (:quantity @data) :on-change (value-handler #(swap! data assoc :quantity %))}]
+              [:button {:type "button" :on-click #(swap! data update :quantity inc)} [sa/Icon {:name "plus"}]]]
+             [:button {:type "button"} [sa/Icon {:name "add to cart"}] "Añadir a la cesta"]]]
+           [:h2 "Test frontend product" (route-param :id)]
+           [:p (:description product)]
+           [:pre (.stringify js/JSON (clj->js product) nil 2)]])]]))])
