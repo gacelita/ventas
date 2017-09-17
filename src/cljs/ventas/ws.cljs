d42ec17c217d9bfdd9b682b451327c326521be27
Joel Sánchez <webmaster@kazer.es>
2017-09-17 13:43:07 +0200

    Refactor ventas.ws

--- src/cljs/ventas/ws.cljs
+++ src/cljs/ventas/ws.cljs
@@ -1,133 +1,105 @@
 (ns ventas.ws
-  (:require [reagent.core :as reagent :refer [atom]]
-            [reagent.session :as session]
-            [re-frame.core :as rf]
-            [bidi.bidi :as bidi]
-            [accountant.core :as accountant]
-            [cljsjs.react-bootstrap]
-            [clojure.string :as s]
-            [ventas.utils.logging :refer [trace debug info warn error]]
-            [cljs.core.async :refer [<! >! put! close! timeout chan]]
-            [chord.client :refer [ws-ch]]
-            [chord.format.fressian :as chord-fressian]
-            [cljs.reader :as edn]
-            [cljs.pprint :as pprint]
-            [ventas.util :as util]
-            [ventas.common.util :as cutil])
+  "Requests and responses, abstracted over websocket communication"
+  (:require [ventas.utils.logging :refer [trace debug info warn error]]
+            [cljs.core.async :refer [<! >! close! chan]]
+            [chord.client :as chord]
+            [chord.format.fressian]
+            [ventas.common.util :as common.util])
   (:require-macros
-   [cljs.core.async.macros :as asyncm :refer (go go-loop)]))
+   [cljs.core.async.macros :refer [go go-loop]]))
 
-(enable-console-print!)
+(def ^:private output-json-channel (atom nil))
+(def ^:private output-binary-channel (atom nil))
+(def ^:private active-request-channels (atom {}))
 
-
-
-(def output-channel (atom nil))
-(def input-channel (atom nil))
-(def output-binary-channel (atom nil))
-(def input-binary-channel (atom nil))
-(def active-channels (atom {}))
-
-
-;; Requests and responses, abstracted over websocket communication
-
-;; Sends a request and calls the callback with the response
-(defn send-request! [{:keys [name params callback request-params]}]
-  (let [ch (chan)
-        id (rand-int 999999)]
-    (debug ::send-request!
-           {:output-channel output-channel
-            :name name
-            :params params
-            :request-params request-params
-            :callback callback})
-    (swap! active-channels assoc id ch)
+(defn send-request!
+  "Sends a request and calls the callback with the response"
+  [{:keys [name params callback]} & {:keys [binary? realtime?]}]
+  (debug ::send-request!
+         {:name name
+          :params params
+          :binary? binary?
+          :realtime? realtime?})
+  (let [request-channel (chan)
+        request-id (gensym "request-")
+        output-channel (if binary? @output-binary-channel @output-json-channel)]
+    (swap! active-request-channels assoc request-id request-channel)
     (go
-      (>! (if (:binary request-params) @output-binary-channel @output-channel) {:type :request :id id :name name :params params})
-      (loop []
-        (let [message (<! ch)]
-          (callback message)
-          (if (:realtime request-params)
-            (recur)
-            (swap! active-channels dissoc id)))))))
+     (>! output-channel
+         {:type :request
+          :id request-id
+          :name name
+          :params params})
+     (loop []
+       (let [message (<! request-channel)]
+         (callback message)
+         (if realtime?
+           (recur)
+           (do (close! request-channel)
+               (swap! active-request-channels dissoc request-id))))))))
 
-;; Puts every response into its channel
-(defn ws-response-dispatch [message]
-  (let [ch (get @active-channels (:id message))]
-    (when-not (nil? ch)
-      (go (>! ch message)))))
+(defn- ws-response-dispatch
+  "Puts a response into its corresponding channel"
+  [message]
+  (let [channel (get @active-request-channels (:id message))]
+    (when channel
+      (go (>! channel message)))))
 
-;; Calls an event handler, if any
-(defmulti ws-event-dispatch (fn [message] (:name message)))
+(defmulti ws-event-dispatch
+  "Calls an event handler, if any"
+  :name)
 
 (declare init)
-(defmethod ws-event-dispatch :server-restarted [message client-id ws-channel]
-  (init (constantly true)))
 
-(defmethod ws-event-dispatch :default [message]
-  (debug "Unhandled event: " message))
+(defmethod ws-event-dispatch :server-restarted [_]
+  (init))
 
+(defmethod ws-event-dispatch :default [event]
+  (debug "Unhandled event: " event))
 
-;; Base websocket communication
-
-;; Receives messages from a server channel and calls
-;; an appropiate dispatcher
-(defn receive-messages! [input-channel server-ch]
+(defn receive-messages! [websocket-channel]
+  "Receives messages from the server and calls an appropiate dispatcher"
   (go-loop []
-    (let [message (cutil/process-input-message (:message (<! server-ch)))]
-      (case (:type message)
+    (let [{:keys [type] :as message} (common.util/process-input-message (:message (<! websocket-channel)))]
+      (case type
         :event (ws-event-dispatch message)
         :response (ws-response-dispatch message)
-        (js/console.warn "Unhandled message: " message))
-      (if (and message (:type message))
+        (warn "Unhandled websocket message" message))
+      (if message
         (recur)
         (init)))))
 
-;; Receives messages from output-channel and sends them
-;; over to the server
-(defn send-messages! [output-channel server-ch]
+(defn- send-messages!
+  "Receives messages from output-channel and send them to the server"
+  [output-channel websocket-channel]
   (go-loop []
-    (when-let [message (cutil/process-output-message (<! output-channel))]
-      (>! server-ch message)
+    (when-let [message (common.util/process-output-message (<! output-channel))]
+      (>! websocket-channel message)
       (recur))))
 
-;; @TODO
-;; Same as receive-messages! but for binary data
-(defn receive-binary-messages! [input-channel server-ch]
-  (go-loop []
-    (let [message (:message (<! server-ch))]
-      (debug "receive-binary-messages!" message)
-      (ws-response-dispatch message)
-      (when message
-        (recur)))
-))
-
-;; @TODO
-;; Same as send-messages! but for binary data
-(defn send-binary-messages! [output-channel server-ch]
-  (go-loop []
-    (when-let [message (<! output-channel)]
-      (debug "send-binary-messages!" message)
-      (>! server-ch message)
-      (recur))))
+(defn- start-websocket [format]
+  {:pre [(#{:fressian :json-kw} format)]}
+  (go
+   (let [channel (chan)
+         {:keys [ws-channel] ws-error :error} (<! (chord/ws-ch "ws://localhost:3450/ws/fressian" {:format format}))]
+     (if ws-error
+       (do
+         (error "Error connecting to the " format " websocket: " ws-error)
+         (>! channel false))
+       (do
+         (reset! output-json-channel
+                 (doto (chan)
+                   (send-messages! ws-channel)))
+         (receive-messages! ws-channel)
+         (>! channel true))))))
 
 (defn init
-  "Start the input and output binary and non-binary channels"
+  "Starts the websockets"
   []
-  (let [ch (chan)]
+  (let [channel (chan)]
     (go
-      (info "Starting channels")
-      (try
-        (let [ws-result (<! (ws-ch "ws://localhost:3450/ws" {:format :json-kw}))
-              ws-binary-result (<! (ws-ch "ws://localhost:3450/binary-ws" {:format :fressian}))]
-          (when (:error ws-result)
-            (throw (js/Error. "Error conectándose al Websocket: " (:error ws-result))))
-          (when (:error ws-binary-result)
-            (throw (js/Error. "Error conectándose al Websocket: " (:error ws-binary-result))))
-          (reset! input-channel (doto (atom []) (receive-messages! (:ws-channel ws-result))))
-          (reset! output-channel (doto (chan) (send-messages! (:ws-channel ws-result))))
-          (reset! input-binary-channel (doto (atom []) (receive-binary-messages! (:ws-channel ws-binary-result))))
-          (reset! output-binary-channel (doto (chan) (send-binary-messages! (:ws-channel ws-binary-result))))
-          (>! ch true))
-       (catch :default e
-         (error "Error de conexión: " e))))
-    ch))
+      (info "Starting websockets")
+      (let [json-result (<! (start-websocket :json-kw))
+            fressian-result (<! (start-websocket :fressian))]
+        (>! channel (and json-result fressian-result))))
+    channel))
