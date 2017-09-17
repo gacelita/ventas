4f55c3de0978d185bcdf8a3e6e3dff0c6381ce90
Joel Sánchez <webmaster@kazer.es>
2017-09-17 14:31:07 +0200

    Fix utils.logging

--- src/cljs/ventas/utils/logging.cljs
+++ src/cljs/ventas/utils/logging.cljs
@@ -4,10 +4,10 @@
   {:pre [(#{:info :debug :warn :error} level)]}
   (case level
     :info (apply js/console.info "INFO [] - " data)
-    :debug (apply js/console.debug data)
-    :warn (apply js/console.warn data)
-    :error (apply js/console.error data)
-    :trace (apply js/console.trace data)))
+    :debug (apply js/console.debug "DEBUG [] - " data)
+    :warn (apply js/console.warn "WARN [] - " data)
+    :error (apply js/console.error "ERROR [] - " data)
+    :trace (apply js/console.trace "TRACE [] - " data)))
 
 (defn info [& data]
   (log :info data))
