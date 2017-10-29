cbfc1b5df2753829cab66adb7eada60fb7f7731e
Joel Sánchez <webmaster@kazer.es>
2017-10-29 13:34:04 +0100

    Add translations

--- src/cljs/ventas/i18n.cljs
+++ src/cljs/ventas/i18n.cljs
@@ -1,9 +1,11 @@
 (ns ventas.i18n
   (:require
-   [tongue.core :as tongue]))
+   [tongue.core :as tongue]
+   [ventas.utils.logging :refer [debug]]))
 
 (def dicts
   {:en_US
+
    {:ventas.pages.admin.products/create-product "Create product"
     :ventas.pages.admin.products/name "Name"
     :ventas.pages.admin.products/email "Email"
@@ -14,7 +16,10 @@
     :ventas.pages.admin.users.edit/page "Edit user"
     :ventas.pages.admin.products/page "Products"
     :ventas.pages.admin.products.edit/page "Edit product"
-    }
+
+    :user.role/administrator "Administrator"
+    :user.role/user "User"}
+
    :tongue/fallback :en_US})
 
 (def ^:private translation-fn (tongue/build-translate dicts))
