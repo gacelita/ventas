bf51e10f2767dddfd4fb419bd9e87aa10bbc51ab
Joel Sánchez <webmaster@kazer.es>
2017-11-01 11:56:30 +0100

    Refactor components.category-list

--- src/cljs/ventas/components/category_list.cljs
+++ src/cljs/ventas/components/category_list.cljs
@@ -1,39 +1,27 @@
 (ns ventas.components.category-list
-  (:require [ventas.util :as util]
-            [re-frame.core :as rf]
-            [clojure.string :as s]
-            [soda-ash.core :as sa]
-            [fqcss.core :refer [wrap-reagent]]
-            [cljs.pprint :refer [pprint]]
-            [ventas.routes :as routes]))
+  (:require
+   [re-frame.core :as rf]
+   [ventas.routes :as routes]))
 
-(rf/reg-sub ::main
-  (fn [db _] (-> db :categories)))
-
-(rf/reg-event-fx ::init
-  (fn [cofx [_]]
-    (js/console.log "initing")
-    {:ws-request {:name :categories/list
-                  :success-fn #(rf/dispatch [:ventas/db [::main] %])}}))
+(def categories-key ::categories)
 
 (rf/reg-event-fx
- ::add
- (fn [{:keys [db local-storage]} [_ item]]
-   {:db (assoc-in db [:categories (:id item)] item)}))
+ ::init
+  (fn [cofx [_]]
+    {:dispatch [:api/categories.list
+                {:success-fn #(rf/dispatch [:ventas.db [categories-key] %])}]}))
 
 (defn category-list []
-  "Category list"
-  (js/console.log "rendering")
-  [:div.category-list
-   (for [[id category] @(rf/subscribe [::main])]
-     (do
-       (js/console.log "category" category)
-       ^{:key id}
-       [:div.category-list__category {:key (:id category)
-                                      :on-click #(routes/go-to :frontend.category :id 1)}
-        (when (:image category)
-          [:img.category-list__image {:src (:url (:image category))}])
-        [:div.category-list__content
-         [:h3 (:name category)]
-         (when (:description category)
-           [:p (:description category)])]]))])
+  (rf/dispatch [::init])
+  (fn []
+    [:div.category-list
+     (let [categories @(rf/subscribe [:ventas/db [categories-key]])]
+       (for [[id {:keys [name description image]}] categories]
+         [:div.category-list__category {:key id
+                                        :on-click #(routes/go-to :frontend.category :id id)}
+          (when image
+            [:img.category-list__image {:src (:url image)}])
+          [:div.category-list__content
+           [:h3 name]
+           (when description
+             [:p description])]]))]))
