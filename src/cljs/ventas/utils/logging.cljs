84f4ef5916644931044e080b49cde17de13e7dfd
Joel Sánchez <webmaster@kazer.es>
2017-12-04 20:34:33 +0100

    Do not force developers to toggle on `verbose` mode for debugging in the JS console, since it comes with a lot of noise

--- src/cljs/ventas/utils/logging.cljs
+++ src/cljs/ventas/utils/logging.cljs
@@ -1,19 +1,30 @@
-(ns ventas.utils.logging)
+(ns ventas.utils.logging
+  "Simple wrapper for js/console")
+
+(def log-level :debug)
+
+(def ^:private log-levels [:trace :debug :info :warn :error])
 
 (defn- log [level data]
-  {:pre [(#{:info :debug :warn :error} level)]}
-  (case level
-    :info (apply js/console.info "INFO [] - " data)
-    :debug (apply js/console.debug "DEBUG [] - " data)
-    :warn (apply js/console.warn "WARN [] - " data)
-    :error (apply js/console.error "ERROR [] - " data)
-    :trace (apply js/console.trace "TRACE [] - " data)))
+  {:pre [(contains? (set log-levels) level)]}
+  (let [index (.indexOf log-levels level)
+        min-index (.indexOf log-levels log-level)]
+    (when (<= min-index index)
+      (case level
+        :trace (apply js/console.log "TRACE [] - " data)
+        :debug (apply js/console.log "DEBUG [] - " data)
+        :info (apply js/console.info "INFO [] - " data)
+        :warn (apply js/console.warn "WARN [] - " data)
+        :error (apply js/console.error "ERROR [] - " data)))))
 
-(defn info [& data]
-  (log :info data))
+(defn trace [& data]
+  (log :trace data))
 
 (defn debug [& data]
   (log :debug data))
+
+(defn info [& data]
+  (log :info data))
 
 (defn warn [& data]
   (log :warn data))
@@ -21,5 +32,3 @@
 (defn error [& data]
   (log :error data))
 
-(defn trace [& data]
-  (log :trace data))
