38e68e5477ffe3f43caadd69f9a543ec984aab5f
Joel Sánchez <webmaster@kazer.es>
2017-12-06 22:46:32 +0100

    Add some color fixtures to the theme

--- src/clj/ventas/themes/clothing/core.clj
+++ src/clj/ventas/themes/clothing/core.clj
@@ -3,7 +3,8 @@
    [ventas.database.entity :as entity]
    [ventas.database.generators :as generators]
    [ventas.theme :as theme]
-   [clojure.spec.alpha :as spec]))
+   [clojure.spec.alpha :as spec]
+   [ventas.entities.i18n :as entities.i18n]))
 
 (theme/register!
  :clothing
@@ -30,7 +31,35 @@
       :image-size/width (* 460 4)
       :image-size/height (* 650 4)
       :image-size/algorithm :image-size.algorithm/crop-and-resize
-      :image-size/entities #{:schema.type/product}}])})
+      :image-size/entities #{:schema.type/product}}
+
+     {:schema/type :schema.type/product.term
+      :product.term/name (entities.i18n/get-i18n-entity {:en_US "Dark green"
+                                                         :es_ES "Verde oscuro"})
+      :product.term/keyword :dark-green
+      :product.term/color "#584838"
+      :product.term/taxonomy [:product.taxonomy/keyword :color]}
+
+     {:schema/type :schema.type/product.term
+      :product.term/name (entities.i18n/get-i18n-entity {:en_US "Dark red"
+                                                         :es_ES "Rojo oscuro"})
+      :product.term/keyword :dark-red
+      :product.term/color "#5f3239"
+      :product.term/taxonomy [:product.taxonomy/keyword :color]}
+
+     {:schema/type :schema.type/product.term
+      :product.term/name (entities.i18n/get-i18n-entity {:en_US "Black"
+                                                         :es_ES "Negro"})
+      :product.term/keyword :black
+      :product.term/color "#252525"
+      :product.term/taxonomy [:product.taxonomy/keyword :color]}
+
+     {:schema/type :schema.type/product.term
+      :product.term/name (entities.i18n/get-i18n-entity {:en_US "Dark blue"
+                                                         :es_ES "Azul oscuro"})
+      :product.term/keyword :dark-blue
+      :product.term/color "#262b3f"
+      :product.term/taxonomy [:product.taxonomy/keyword :color]}])})
 
 (spec/def :product.term/color ::generators/string)
 
