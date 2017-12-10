2dc96b6d6bec85f256e5056bd30070b7e772b65e
Joel Sánchez <webmaster@kazer.es>
2017-12-10 21:49:55 +0100

    Create :country.group and :shipping-method

--- src/clj/ventas/entities/country.clj
+++ src/clj/ventas/entities/country.clj
@@ -27,3 +27,36 @@
                                                     :es_ES "España"})}
      {:country/name (entities.i18n/get-i18n-entity {:en_US "United States"
                                                     :es_ES "Estados Unidos"})}])})
+
+
+(spec/def :schema.type/country.group
+  (spec/keys :req [:country.group/name]))
+
+(entity/register-type!
+ :country.group
+ {:attributes
+  [{:db/ident :country.group/name
+    :db/valueType :db.type/ref
+    :db/cardinality :db.cardinality/one}
+   {:db/ident :country.group/keyword
+    :db/valueType :db.type/keyword
+    :db/cardinality :db.cardinality/one}]
+
+  :dependencies
+  #{:i18n}
+
+  :autoresolve? true
+
+  :fixtures
+  (fn []
+    [{:country.group/name (entities.i18n/get-i18n-entity {:en_US "Europe"
+                                                          :es_ES "Europa"})
+      :country.group/keyword :europe}
+
+     {:country.group/name (entities.i18n/get-i18n-entity {:en_US "North America"
+                                                          :es_ES "Norteamérica"})
+      :country.group/keyword :north-america}
+
+     {:country.group/name (entities.i18n/get-i18n-entity {:en_US "Europe (non-EU)"
+                                                          :es_ES "Europe (fuera de la UE)"})
+      :country.group/keyword :europe-non-eu}])})
---
+++ src/clj/ventas/entities/shipping_method.clj
@@ -0,0 +1,99 @@
+(ns ventas.entities.shipping-method
+  (:require
+   [clojure.spec.alpha :as spec]
+   [ventas.database.generators :as generators]
+   [ventas.entities.i18n :as entities.i18n]
+   [ventas.database.entity :as entity]))
+
+(spec/def :shipping-method.price/min-value ::generators/bigdec)
+
+(spec/def :shipping-method.price/max-value ::generators/bigdec)
+
+(spec/def :shipping-method.price/amount
+  (spec/with-gen ::entity/ref #(entity/ref-generator :amount)))
+
+(spec/def :shipping-method.price/country-groups
+  (spec/with-gen ::entity/refs #(entity/refs-generator :country.group)))
+
+(spec/def :schema.type/shipping-method.price
+  (spec/keys :req [:shipping-method.price/min-value
+                   :shipping-method.price/max-value
+                   :shipping-method.price/amount
+                   :shipping-method.price/country-groups]))
+
+(entity/register-type!
+ :shipping-method.price
+ {:attributes
+  [{:db/ident :shipping-method.price/min-value
+    :db/valueType :db.type/bigdec
+    :db/cardinality :db.cardinality/one}
+
+   {:db/ident :shipping-method.price/max-value
+    :db/valueType :db.type/bigdec
+    :db/cardinality :db.cardinality/one}
+
+   {:db/ident :shipping-method.price/amount
+    :db/valueType :db.type/ref
+    :db/cardinality :db.cardinality/one}
+
+   {:db/ident :shipping-method.price/country-groups
+    :db/valueType :db.type/ref
+    :db/cardinality :db.cardinality/many}]
+
+  :dependencies
+  #{:amount :country.group}
+
+  :autoresolve? true})
+
+
+(spec/def :shipping-method/name ::entities.i18n/ref)
+
+(spec/def :shipping-method/default? boolean?)
+
+(spec/def :shipping-method/manipulation-fee
+  (spec/with-gen ::entity/ref #(entity/ref-generator :amount)))
+
+(spec/def :shipping-method/pricing #{:shipping-method.pricing/price
+                                     :shipping-method.pricing/weight})
+
+(spec/def :shipping-method/prices
+  (spec/with-gen ::entity/refs #(entity/refs-generator :shipping-method.price)))
+
+(spec/def :schema.type/shipping-method
+  (spec/keys :req [:shipping-method/name
+                   :shipping-method/default?
+                   :shipping-method/manipulation-fee
+                   :shipping-method/pricing
+                   :shipping-method/prices]))
+
+(entity/register-type!
+ :shipping-method
+ {:attributes
+  [{:db/ident :shipping-method/name
+    :db/valueType :db.type/ref
+    :db/cardinality :db.cardinality/one}
+
+   {:db/ident :shipping-method/default?
+    :db/valueType :db.type/boolean
+    :db/cardinality :db.cardinality/one}
+
+   {:db/ident :shipping-method/manipulation-fee
+    :db/valueType :db.type/ref
+    :db/cardinality :db.cardinality/one}
+
+   {:db/ident :shipping-method/pricing
+    :db/valueType :db.type/ref
+    :db/cardinality :db.cardinality/one}
+
+   {:db/ident :shipping-method.pricing/price}
+   {:db/ident :shipping-method.pricing/weight}
+
+   {:db/ident :shipping-method/prices
+    :db/valueType :db.type/ref
+    :db/cardinality :db.cardinality/many
+    :db/isComponent true}]
+
+  :dependencies
+  #{:i18n :amount :shipping-method.price}
+
+  :autoresolve? true})
