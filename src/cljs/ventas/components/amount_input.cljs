3e487e20e403c6d6e1a430343f3cea8f839bb57b
Joel Sánchez <webmaster@kazer.es>
2018-01-07 13:50:33 +0100

    Unify amount-input and i18n-input to edit the entities from their ID

--- src/cljs/ventas/components/amount_input.cljs
+++ src/cljs/ventas/components/amount_input.cljs
@@ -2,6 +2,7 @@
   (:require
    [ventas.components.base :as base]
    [ventas.events :as events]
+   [ventas.events.backend :as backend]
    [re-frame.core :as rf]))
 
 (def state-key ::state)
@@ -10,11 +11,8 @@
  ::set-currency
  (fn [{:keys [db]} [_ id currency-id callback]]
    {:pre [(fn? callback) (number? currency-id) id]}
-   (let [currency (->> (get-in db [:admin :currencies])
-                       (filter #(= (:id %) currency-id))
-                       (first))
-         value (get-in db [state-key id])
-         new-value (assoc value :currency currency)]
+   (let [new-value (-> (get-in db [state-key id])
+                       (assoc :amount/currency currency-id))]
      (callback new-value)
      {:db (assoc-in db [state-key id] new-value)})))
 
@@ -23,7 +21,7 @@
  (fn [{:keys [db]} [_ id v callback]]
    {:pre [(fn? callback) (number? v) id]}
    (let [new-state (-> (get-in db [state-key id])
-                       (assoc :value v))]
+                       (assoc :amount/value v))]
      (callback new-state)
      {:db (assoc-in db [state-key id] new-state)})))
 
@@ -37,18 +35,36 @@
                       {:text (:symbol v)
                        :value (:id v)})
                     @(rf/subscribe [::events/db [:admin :currencies]]))
-      :default-value (:id currency)
+      :default-value currency
       :on-change #(rf/dispatch [::set-currency id (js/parseFloat (.-value %2)) on-change])}]]])
 
-(defn input [{:keys [amount control label culture on-change]}]
+(rf/reg-event-fx
+ ::init
+ (fn [_ [_ id amount]]
+   {:dispatch [::backend/admin.entities.find
+               {:params {:id amount}
+                :success [::init.next id]}]}))
+
+(rf/reg-event-db
+ ::init.next
+ (fn [db [_ id data]]
+   (assoc-in db [state-key id] (-> data
+                                   (dissoc :db/id)))))
+
+(defn- input* [{:keys [amount control label culture on-change]}]
   (let [id (gensym)]
-    (rf/dispatch [::events/db [state-key id] amount])
+    (when amount
+      (rf/dispatch [::init id amount]))
     (fn []
-      (let [{:keys [value currency]} amount]
-        [base/form-input {:label label}
+      (let [{:amount/keys [value currency] :as state} @(rf/subscribe [::events/db [state-key id]])]
+        [base/form-input {:label label :key (boolean state)}
          [dropdown {:currency currency
                     :id id
                     :on-change on-change}]
          [(or control :input)
           {:default-value value
            :on-change #(rf/dispatch [::set-value id (js/parseFloat (-> % .-target .-value)) on-change])}]]))))
+
+(defn input [{:keys [amount] :as args}]
+  [input*
+   (assoc args :key (boolean amount))])
--- src/cljs/ventas/components/i18n_input.cljs
+++ src/cljs/ventas/components/i18n_input.cljs
@@ -12,12 +12,19 @@
  (fn [_ [_ id focus]]
    {:dispatch [::events/db [state-key id :focused?] focus]}))
 
+(defn- to-i18n-entity [data]
+  {:schema/type :schema.type/i18n
+   :i18n/translations (for [[culture-kw text] data]
+                        [{:schema/type :schema.type/i18n.translation
+                          :i18n.translation/value text
+                          :i18n.translation/culture [:i18n.culture/keyword culture-kw]}])})
+
 (rf/reg-event-fx
  ::on-change
  (fn [{:keys [db]} [_ {:keys [culture translation callback id]}]]
    (let [data (get-in db [state-key id :data])
          new-data (assoc data culture translation)]
-     (callback new-data)
+     (callback (to-i18n-entity new-data))
      {:db (assoc-in db [state-key id :data] new-data)})))
 
 (defn- culture-view [{:keys [translation culture id label on-change control]}]
@@ -32,28 +39,45 @@
                                 :translation (-> % .-target .-value)
                                 :callback on-change}])}]])
 
-(defn input [{:keys [label value on-change culture control]}]
-  (let [id (str (gensym))]
-    (rf/dispatch [::events/db [state-key id :data] value])
+(rf/reg-event-fx
+ ::init
+ (fn [_ [_ id entity]]
+   {:dispatch [::backend/admin.entities.find-json
+               {:params {:id entity}
+                :success [::init.next id]}]}))
+
+(rf/reg-event-db
+ ::init.next
+ (fn [db [_ id data]]
+   (assoc-in db [state-key id :value] (-> data
+                                          (dissoc :db/id)))))
+
+(defn- input* [{:keys [label entity on-change culture control]}]
+  (let [id (gensym)]
+    (when entity
+      (rf/dispatch [::init id entity]))
     (fn []
-      (let [{:keys [focused?]} @(rf/subscribe [::events/db [state-key id]])
+      (let [{:keys [focused? value]} @(rf/subscribe [::events/db [state-key id]])
             cultures (map :value @(rf/subscribe [::events/db :cultures]))]
-        [:div.i18n-input {:class (when focused? "i18n-input--focused")
+        [:div.i18n-input {:key (boolean value)
+                          :class (when focused? "i18n-input--focused")
                           :on-focus #(rf/dispatch [::set-focus id true])
                           :on-blur #(rf/dispatch [::set-focus id false])}
-         (let [translation (get value culture)]
-           [culture-view {:culture culture
-                          :control control
-                          :translation translation
-                          :id id
-                          :on-change on-change
-                          :label label}])
+         [culture-view {:culture culture
+                        :control control
+                        :translation (get value culture)
+                        :id id
+                        :on-change on-change
+                        :label label}]
          [:div.i18n-input__cultures
           (for [culture (rest cultures)]
-            (let [translation (get value culture)]
-              [culture-view {:culture culture
-                             :control control
-                             :translation translation
-                             :id id
-                             :on-change on-change
-                             :key culture}]))]]))))
+            [culture-view {:culture culture
+                           :control control
+                           :translation (get value culture)
+                           :id id
+                           :on-change on-change
+                           :key culture}])]]))))
+
+(defn input [{:keys [entity] :as args}]
+  [input*
+   (assoc args :key (boolean entity))])
