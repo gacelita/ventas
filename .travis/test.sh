776bcdc4c979e0363525cddd72ac8923f7ccc483
Joel Sánchez <webmaster@kazer.es>
2018-01-11 00:45:02 +0100

    Add karma to test.sh

--- .travis/test.sh
+++ .travis/test.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env sh
 
 cp resources/config.example.edn resources/config.edn
+
+npm install karma karma-cljs-test
+npm install -g karma-cli
 npm install karma-chrome-launcher
 
 lein test
