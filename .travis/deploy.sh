4ff4578b8595a2313902368199916e8a594b29cb
Joel Sánchez <webmaster@kazer.es>
2018-01-11 15:30:53 +0100

    Finish deployment script

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -2,6 +2,11 @@
 set -e
 ssh travis@kazer.es <<'ENDSSH'
 cd ventas
+echo "Pulling from github"
+git pull
+echo "Building Docker images"
 ./build.sh
+echo "Restarting service"
 systemctl --user restart ventas
+echo "Done"
 ENDSSH
