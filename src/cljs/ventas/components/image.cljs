cfbe99b606f8ffc61c422bcf073edb522b16be78
Joel Sánchez <webmaster@kazer.es>
2018-01-23 19:45:59 +0100

    Add ventas.components.image and :image-sizes.list

---
+++ src/cljs/ventas/components/image.cljs
@@ -0,0 +1,23 @@
+(ns ventas.components.image
+  (:require
+   [re-frame.core :as rf]
+   [reagent.core :as reagent]
+   [ventas.events :as events]
+   [ventas.components.base :as base]))
+
+(defn image [id size]
+  {:pre [id (keyword? size)]}
+  (rf/dispatch [::events/image-sizes.list])
+  (let [loaded? (reagent/atom false)]
+    (fn [id size]
+      (when-let [{:keys [width height]} @(rf/subscribe [::events/db [:image-sizes size]])]
+        [:div.image-component {:style {:width width
+                                       :height height}}
+         [:div.image-component__dimmer (when @loaded? {:style {:display "none"}})
+          [base/dimmer {:active true
+                        :inverted true}
+           [base/loader {:inverted true}
+            "Loading"]]]
+         [:img {:style (when-not @loaded? {:display "none"})
+                :on-load #(reset! loaded? true)
+                :src (str "images/" id "/resize/" (name size))}]]))))
