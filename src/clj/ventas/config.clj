2fb522110f6fd6a4fdb667256e63feb27ffaf3ef
Joel Sánchez <webmaster@kazer.es>
2018-01-29 22:29:46 +0100

    Dynamic loading of configuration (mainly done for production)

--- src/clj/ventas/config.clj
+++ src/clj/ventas/config.clj
@@ -2,7 +2,9 @@
   (:refer-clojure :exclude [get set])
   (:require
    [clojure.core :as clj]
-   [cprop.core :refer [load-config]]))
+   [cprop.core :as cprop]
+   [mount.core :refer [defstate]]
+   [taoensso.timbre :as timbre]))
 
 (def ^:private defaults
   {:server {:port 3450
@@ -15,12 +17,25 @@
    :cljs-port 3001
    :strict-classloading false})
 
-(defonce ^:private config (atom (merge defaults (load-config))))
+(defn- load-config []
+  (merge defaults (cprop/load-config)))
+
+(defonce ^:private config (atom (load-config)))
+
+(defstate config-loader
+  :start
+  (let [config-data (load-config)]
+    (timbre/info "Loading configuration")
+    (timbre/debug config-data)
+    (reset! config config-data)))
 
 (defn set
-  [k v]
-  {:pre [(keyword? k)]}
-  (swap! config assoc k v))
+  [k-or-ks v]
+  {:pre [(or (keyword? k-or-ks) (sequential? k-or-ks))]}
+  (let [ks (if (keyword? k-or-ks)
+             [k-or-ks]
+             k-or-ks)]
+    (swap! config assoc-in ks v)))
 
 (defn get [& ks]
   (get-in @config ks))
