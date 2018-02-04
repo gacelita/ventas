049df39d27c1e271303cf25459a392af50caaf8c
Joel Sánchez <webmaster@kazer.es>
2018-02-04 14:57:36 +0100

    Write bash script for recreating the demo db on deploy

--- .travis/ssh_script.sh
+++ .travis/ssh_script.sh
@@ -10,7 +10,7 @@ set -e
 cd /opt/ventas
 
 echo "Pulling from github"
-git pull
+git pull --rebase --prune
 
 echo "Installing bower deps"
 bower install
@@ -23,4 +23,14 @@ docker build -t datomic datomic &&
 echo "Restarting service" &&
 docker-compose down &&
 docker-compose up -d &&
-echo "Done"
+echo "Deploy done, executing REPL commands..." &&
+until [ "`docker inspect -f {{.State.Running}} ventas_ventas_1`"=="true" ]; do
+    sleep 1;
+done &&
+CONTAINER_IP=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' ventas_ventas_1); \
+lein repl :connect $CONTAINER_IP:4001 << ENDREPL
+(ventas.database.seed/seed :recreate? true)
+(mount.core/stop #'ventas.search/indexer)
+(mount.core/start #'ventas.search/indexer)
+(ventas.search/reindex)
+ENDREPL
