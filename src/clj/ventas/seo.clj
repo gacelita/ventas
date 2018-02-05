eeaba737cd54af7c13946075844da304d1e06544
Joel Sánchez <webmaster@kazer.es>
2018-02-06 00:42:57 +0100

    Refactor ventas.seo

--- src/clj/ventas/seo.clj
+++ src/clj/ventas/seo.clj
@@ -33,25 +33,34 @@
      (do (<! (core.async/timeout 400))
          (recur)))))
 
+(defn- server-uri []
+  (let [{:keys [port host]} (config/get :server)]
+    (str "http://" host ":" port)))
+
+(defn prerender [route & [driver]]
+  (timbre/debug "Prerendering route" route)
+  (go
+    (let [driver (or driver (doto (etaoin/chrome-headless)
+                              (etaoin/go (server-uri))))]
+      (etaoin/js-execute driver (str "ventas.routes.js_go_to("
+                                     (cheshire/encode route)
+                                     ");"))
+      (let [pending-ch (no-pending-requests-async driver)
+            [message ch] (core.async/alts! [pending-ch
+                                            (core.async/timeout 4000)])
+            url (subs (etaoin/js-execute driver "return document.location.pathname;") 1)
+            path (str (paths/resolve ::paths/rendered)
+                      "/"
+                      (if (empty? url) "index" url)
+                      ".html")]
+        (io/make-parents path)
+        (spit path
+              (etaoin/js-execute driver "return document.getElementById('app').innerHTML;"))))))
+
 (defn prerender-all []
-  (let [{:keys [port host]} (config/get :server)
-        driver (etaoin/chrome-headless)]
-    (etaoin/go driver (str "http://" host ":" port))
+  (let [driver (doto (etaoin/chrome-headless)
+                 (etaoin/go (server-uri)))]
     (go
       (doseq [route (get-routes)]
-        (timbre/debug "Prerendering route" route)
-        (etaoin/js-execute driver (str "ventas.routes.js_go_to("
-                                       (cheshire/encode route)
-                                       ");"))
-        (let [pending-ch (no-pending-requests-async driver)
-              [message ch] (core.async/alts! [pending-ch
-                                              (core.async/timeout 4000)])
-              url (subs (etaoin/js-execute driver "return document.location.pathname;") 1)
-              path (str (paths/resolve ::paths/rendered)
-                        "/"
-                        (if (empty? url) "index" url)
-                        ".html")]
-          (io/make-parents path)
-          (spit path
-                (etaoin/js-execute driver "return document.getElementById('app').innerHTML;"))))
+        (<! (prerender route driver)))
       (etaoin/quit driver))))
