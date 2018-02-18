86f34792f13046fbcf7dd3de4b99aadcdab84533
Joel Sánchez <webmaster@kazer.es>
2018-02-18 14:55:04 +0100

    Much better logging. The appender is now thread-safe and padded multilines are applied universally.

--- src/clj/ventas/logging.clj
+++ src/clj/ventas/logging.clj
@@ -5,49 +5,58 @@
    [io.aviso.ansi :as clansi]
    [taoensso.timbre :as timbre]))
 
+(defn- try-parsing [data]
+  (try
+    (let [clj (read-string (str "[" data "]"))]
+      (if (seq (filter symbol? clj))
+        data
+        clj))
+    (catch Throwable _
+      data)))
+
 (defn- timbre-logger
   ([data]
    (timbre-logger nil data))
   ([opts data]
    (let [{:keys [no-stacktrace?]} opts
-         {:keys [level ?err msg_ ?ns-str ?file ?line]} data
-         whats (try
-                 (read-string (str "[" (force msg_) "]"))
-                 (catch Throwable e
-                   (force msg_)))
-         whats (if (every? identity (map #(or (symbol? %)
-                                              (boolean? %)
-                                              (string? %)
-                                              (keyword? %)
-                                              (number? %)) whats))
-                 (apply str (interpose " " whats))
-                 whats)]
-     (when (or (not= level :debug)
-               (str/starts-with? (or ?ns-str ?file) "ventas"))
-       (merge
-        {:level (str/upper-case (name level))
-         :where (str "[" (or ?ns-str ?file "?") ":" (or ?line "?") "]")
-         :whats whats}
-        (when-not no-stacktrace?
-          (when ?err
-            {:stacktrace (timbre/stacktrace ?err opts)})))))))
+         {:keys [level ?err msg_ ?ns-str ?file ?line]} data]
+     (merge
+      {:level (str/upper-case (name level))
+       :where (str "[" (or ?ns-str ?file "?") ":" (or ?line "?") "]")
+       :value (try-parsing (force msg_))}
+      (when-not no-stacktrace?
+        (when ?err
+          {:stacktrace (timbre/stacktrace ?err opts)}))))))
+
+(defn- pad-lines [multiline-str n]
+  (str
+   (-> multiline-str
+       (str/replace "\n" (str "\n" (str/join (repeat n " "))))
+       (str/trimr))))
 
 (defn- timbre-appender-fn [{:keys [output_]}]
-  (let [{:keys [level where whats]} (force output_)
-        info-line (str level " " where " - ")]
-    (when whats
-      (print (case level
-               "ERROR" (clansi/red info-line)
-               (clansi/green info-line)))
-      (if (string? whats)
-        (println whats)
-        (doseq [[idx what] (map-indexed vector whats)]
-          (-> (with-out-str (pprint/pprint what))
-              (str/replace "\n" (str "\n" (str/join (repeat (count info-line) " "))))
-              (str/trimr)
-              (println))
-          (when (not= (dec (count whats)) idx)
-            (print (str/join (repeat (count info-line) " ")))))))))
+  (let [{:keys [level where value]} (force output_)
+        info-str (str level " " where " - ")]
+    (when (if (string? value)
+            (not (str/blank? value))
+            value)
+      (println
+       (str
+        (case level
+          "ERROR" (clansi/red info-str)
+          (clansi/green info-str))
+        (let [pad-n (count info-str)]
+          (if (string? value)
+            (pad-lines value pad-n)
+            (reduce (fn [out [idx itm]]
+                      (str out
+                           (pad-lines (with-out-str (pprint/pprint itm))
+                                      pad-n)
+                           (when (not= (dec (count value)) idx)
+                             (str "\n"
+                                  (str/join (repeat pad-n " "))))))
+                    ""
+                    (map-indexed vector value)))))))))
 
 (timbre/merge-config!
  {:level :debug
@@ -55,7 +64,9 @@
   :ns-blacklist ["datomic.*"
                  "io.netty.*"
                  "org.apache.http.*"
-                 "org.eclipse.aether.internal.impl.*"]
+                 "org.eclipse.aether.internal.impl.*"
+                 "org.apache.kafka.clients.NetworkClient"
+                 "org.apache.kafka.clients.consumer.internals.*"]
   :appenders {:println {:enabled? false}
               :pprint-appender {:enabled?   true
                                 :async?     false
