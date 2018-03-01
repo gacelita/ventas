fc054bca48c9c388a3dba0e012050bb6b622ba56
Joel Sánchez <webmaster@kazer.es>
2018-03-01 01:07:07 +0100

    First try at Travis<->Rancher

--- .travis/ssh_script.sh
+++ .travis/ssh_script.sh
@@ -18,14 +18,20 @@ bower install
 echo "Building Docker images"
 
 lein uberjar &&
-docker build -t ventas . &&
-docker build -t datomic datomic &&
+docker build -t joelsanchez/ventas:latest . &&
+docker build -t joelsanchez/datomic:latest datomic &&
+docker login --username joelsanchez --password DOCKER_PASSWORD &&
+docker push joelsanchez/ventas:latest && 
+docker push joelsanchez/ventas-datomic:latest &&
 echo "Restarting service" &&
-docker-compose down &&
-docker-compose up -d &&
+source /etc/rancher &&
+rancher-compose --env-file .env -f docker-compose.prod.yml down ventas && 
+rancher-compose --env-file .env -f docker-compose.prod.yml up -d &&
 echo "Deploy done, executing REPL commands..." &&
 sleep 60 &&
-CONTAINER_IP=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' ventas_ventas_1); \
+CONTAINER_NAME=$(docker ps --filter "label=io.rancher.stack_service.name=ventas/ventas" --format '{{.ID}}'); \
+CONTAINER_IP_MASKED=$(docker inspect -f "{{ index .Config.Labels \"io.rancher.container.ip\"}}" $CONTAINER_NAME); \
+CONTAINER_IP=${CONTAINER_IP_MASKED%/16}
 lein repl :connect ${CONTAINER_IP}:4001 << ENDREPL
 (ventas.database.seed/seed :recreate? true)
 (mount.core/stop #'ventas.search/indexer)
