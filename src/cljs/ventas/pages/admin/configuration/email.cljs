a43eddcd8430e3b0d324d2a556486c5ad776905e
Joel Sánchez <webmaster@kazer.es>
2018-03-09 20:48:56 +0100

    Use the new form component in configuration.email, use notificator/notify-saved too

--- src/cljs/ventas/pages/admin/configuration/email.cljs
+++ src/cljs/ventas/pages/admin/configuration/email.cljs
@@ -21,69 +21,54 @@
  (fn [{:keys [db]} _]
    {:dispatch [::backend/admin.configuration.set
                {:params (get-in db [state-key :form])
-                :success ::submit.next}]}))
-
-(rf/reg-event-fx
- ::submit.next
- (fn [_ _]
-   {:dispatch [::notificator/add {:message (i18n ::saved)
-                                  :theme "success"}]}))
+                :success [::notificator/notify-saved]}]}))
 
 (defn- field [{:keys [key] :as args}]
   [form/field (merge args
                      {:db-path [state-key]
                       :label (i18n (ns-kw key))})])
 
-(defn- form [data]
-  [base/segment {:color "orange"
-                 :title (i18n ::page)}
-   [base/form {:on-submit (utils.ui/with-handler #(rf/dispatch [::submit]))}
-
-    [field {:key :email.from
-            :label (i18n ::email.from)}]
-
-    [field {:key :email.encryption.enabled
-            :label (i18n ::email.from)
-            :type :toggle}]
-
-    (when (:email.encryption.enabled data)
-      [field {:key :email.encryption.type
-              :label (i18n ::email.from)
-              :type :radio
-              :options [{:id "ssl"
-                         :name (i18n ::ssl)}
-                        {:id "tls"
-                         :name (i18n ::tls)}]}])
-
-    [field {:key :email.smtp.enabled
-            :label (i18n ::email.from)
-            :type :toggle}]
-
-    (when (:email.smtp.enabled data)
-      [:div
-
-       [field {:key :email.smtp.host
-               :label (i18n ::email.from)}]
-
-       [field {:key :email.smtp.port
-               :label (i18n ::email.from)
-               :type :number}]
-
-       [field {:key :email.smtp.user
-               :label (i18n ::email.from)}]
-
-       [field {:key :email.smtp.password
-               :label (i18n ::email.from)
-               :type :password}]])
-
-    [base/divider {:hidden true}]
-
-    [base/form-button
-     {:type "submit"}
-     (i18n ::submit)]]])
-
 (defn- content []
-  [form/form [state-key] form])
+  [form/form [state-key]
+   (let [data @(rf/subscribe [::form/data [state-key]])]
+     [base/segment {:color "orange"
+                    :title (i18n ::page)}
+      [base/form {:on-submit (utils.ui/with-handler #(rf/dispatch [::submit]))}
+
+       [field {:key :email.from}]
+
+       [field {:key :email.encryption.enabled
+               :type :toggle}]
+
+       (when (:email.encryption.enabled data)
+         [field {:key :email.encryption.type
+                 :type :radio
+                 :options [{:id "ssl"
+                            :name (i18n ::ssl)}
+                           {:id "tls"
+                            :name (i18n ::tls)}]}])
+
+       [field {:key :email.smtp.enabled
+               :type :toggle}]
+
+       (when (:email.smtp.enabled data)
+         [:div
+
+          [field {:key :email.smtp.host}]
+
+          [field {:key :email.smtp.port
+                  :type :number}]
+
+          [field {:key :email.smtp.user}]
+
+          [field {:key :email.smtp.password
+                  :type :password}]])
+
+       [base/divider {:hidden true}]
+
+       [base/form-button
+        {:type "submit"}
+        (i18n ::submit)]]])])
 
 (defn- page []
   [admin.skeleton/skeleton
