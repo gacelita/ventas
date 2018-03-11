e70b66d0631add4c419345c9f2397e9dc0cf0556
Joel Sánchez <webmaster@kazer.es>
2018-03-11 17:32:41 +0100

    Add a test for ventas.server.ws

---
+++ test/clj/ventas/server/ws_test.clj
@@ -0,0 +1,46 @@
+(ns ventas.server.ws-test
+  (:require
+   [clojure.test :refer [deftest testing is use-fixtures]]
+   [ventas.server.ws :as sut]
+   [clojure.core.async :as core.async :refer [go <! >!]]
+   [chord.channels :refer [bidi-ch]]
+   [ventas.utils :as utils]))
+
+(use-fixtures :once #(do (sut/start!)
+                         (%)))
+
+(deftest call-handler-with-user
+  (defmethod sut/handle-request :call-handler-with-user-test [request {:keys [session]}]
+    (is (= {:name :call-handler-with-user-test
+            :params {:some :data}}
+           request))
+    (is (= @session {:user 1})))
+  (sut/call-handler-with-user :call-handler-with-user-test
+                              {:some :data}
+                              {:schema/type :schema.type/user
+                               :db/id 1}))
+
+(deftest handle-json-messages
+  (let [test-message {:name :handle-json-messages-test
+                      :id :test-request
+                      :params [:test :data]
+                      :type :request}]
+    (defmethod sut/handle-request :handle-json-messages-test [message {:keys [channel client-id request session]}]
+      (is (= test-message message))
+      (is (utils/chan? channel))
+      (is (uuid? client-id))
+      (is (= [:ws-channel] (keys request)))
+      (is (= @session {})))
+
+    (let [read-ch (core.async/chan)
+          write-ch (core.async/chan)
+          ch (go
+               (sut/handle-messages :json {:ws-channel (bidi-ch read-ch write-ch)})
+               (>! read-ch {:message test-message})
+               (is (= {":data" true
+                       ":id" ":test-request"
+                       ":realtime?" false
+                       ":success" true
+                       ":type" ":response"}
+                      (<! write-ch))))]
+      (core.async/alts!! [ch (core.async/timeout 1000)]))))
