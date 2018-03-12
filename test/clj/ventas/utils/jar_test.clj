bc7cd6df0ebb3fed29d54a88d125a9b9cb20d164
Joel Sánchez <webmaster@kazer.es>
2018-03-12 22:39:30 +0100

    Add tests for ventas.utils.jar

--- test/clj/ventas/utils/jar_test.clj
+++ test/clj/ventas/utils/jar_test.clj
@@ -1,2 +1,15 @@
 (ns ventas.utils.jar-test
-  (:require [clojure.test :refer :all]))
+  (:require
+   [clojure.test :refer [deftest is testing]]
+   [clojure.string :as str]
+   [ventas.utils.jar :as sut]))
+
+(def example-jar "org/clojure/clojure/1.9.0/clojure-1.9.0.jar")
+
+(deftest list-resources
+  (is (contains? (->> (clojure.java.classpath/classpath-jarfiles)
+                      (filter #(str/includes? (.getName %) example-jar))
+                      (first)
+                      (sut/list-resources)
+                      (set))
+                 "META-INF/maven/org.clojure/clojure/pom.xml")))
