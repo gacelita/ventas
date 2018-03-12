d2469905c8f8310eccadbbc539c944cb330407c4
Joel Sánchez <webmaster@kazer.es>
2018-03-12 23:35:22 +0100

    Avoid implicit require

--- test/clj/ventas/utils/jar_test.clj
+++ test/clj/ventas/utils/jar_test.clj
@@ -2,12 +2,13 @@
   (:require
    [clojure.test :refer [deftest is testing]]
    [clojure.string :as str]
+   [clojure.java.classpath :as classpath]
    [ventas.utils.jar :as sut]))
 
 (def example-jar "org/clojure/clojure/1.9.0/clojure-1.9.0.jar")
 
 (deftest list-resources
-  (is (contains? (->> (clojure.java.classpath/classpath-jarfiles)
+  (is (contains? (->> (classpath/classpath-jarfiles)
                       (filter #(str/includes? (.getName %) example-jar))
                       (first)
                       (sut/list-resources)
