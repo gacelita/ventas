75ae2c27475431a07967bc822fa67489b95216dc
Joel Sánchez <webmaster@kazer.es>
2018-03-16 01:00:13 +0100

    Switch state/name from string to i18n ref

--- src/clj/ventas/entities/state.clj
+++ src/clj/ventas/entities/state.clj
@@ -2,9 +2,9 @@
   (:require
    [clojure.spec.alpha :as spec]
    [ventas.database.entity :as entity]
-   [ventas.database.generators :as generators]))
+   [ventas.entities.i18n :as entities.i18n]))
 
-(spec/def :state/name ::generators/string)
+(spec/def :state/name ::entities.i18n/ref)
 
 (spec/def :schema.type/state
   (spec/keys :req [:state/name]))
@@ -13,13 +13,18 @@
  :state
  {:attributes
   [{:db/ident :state/name
-    :db/valueType :db.type/string
-    :db/cardinality :db.cardinality/one}]
+    :db/valueType :db.type/ref
+    :db/cardinality :db.cardinality/one
+    :ventas/refEntityType :i18n}]
 
   :autoresolve? true
+  :dependencies #{:i18n}
 
   :fixtures
   (fn []
-    [{:state/name "Madrid"}
-     {:state/name "Barcelona"}
-     {:state/name "León"}])})
+    [{:state/name (entities.i18n/get-i18n-entity {:en_US "Madrid"
+                                                  :es_ES "Madrid"})}
+     {:state/name (entities.i18n/get-i18n-entity {:en_US "Barcelona"
+                                                  :es_ES "Barcelona"})}
+     {:state/name (entities.i18n/get-i18n-entity {:en_US "León"
+                                                  :es_ES "León"})}])})
