64cbf7a60d88783f7136699b0216173ed7d25e8e
Joel Sánchez <webmaster@kazer.es>
2018-04-21 00:56:24 +0200

    Update table in plugins

--- src/cljs/ventas/pages/admin/plugins.cljs
+++ src/cljs/ventas/pages/admin/plugins.cljs
@@ -11,8 +11,8 @@
 
 (rf/reg-event-fx
  ::fetch
- (fn [{:keys [db]} [_ {:keys [state-path]}]]
-   (let [{:keys [page items-per-page sort-direction sort-column]} (get-in db state-path)]
+ (fn [{:keys [db]} [_ state-path]]
+   (let [{:keys [page items-per-page sort-direction sort-column]} (table/get-state db state-path)]
      {:dispatch [::backend/admin.plugins.list
                  {:success ::fetch.next
                   :params {:pagination {:page page
@@ -24,26 +24,29 @@
  ::fetch.next
  (fn [db [_ {:keys [items total]}]]
    (-> db
-       (assoc-in [state-key :plugins] items)
+       (assoc-in [state-key :table :rows] items)
        (assoc-in [state-key :table :total] total))))
 
 (defn- content []
   [:div.admin-plugins__table
-   [table/table
-    {:init-state {:sort-column :id}
-     :state-path [state-key :table]
-     :data-path [state-key :plugins]
-     :fetch-fx ::fetch
-     :columns [{:id :name
-                :label (i18n ::name)}]}]])
+   [table/table [state-key :table]]])
 
 (defn- page []
   [admin.skeleton/skeleton
    [:div.admin__default-content.admin-plugins__page
     [content]]])
 
+(rf/reg-event-fx
+ ::init
+ (fn [_ _]
+   {:dispatch [::table/init [state-key :table]
+               {:fetch-fx [::fetch]
+                :columns [{:id :name
+                           :label (i18n ::name)}]}]}))
+
 (routes/define-route!
   :admin.plugins
   {:name ::page
    :url "plugins"
-   :component page})
+   :component page
+   :init-fx [::init]})
