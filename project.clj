ecd93e67202cf42941c594cdf4674301c1187bbe
Joel Sánchez <webmaster@kazer.es>
2018-05-29 22:22:52 +0200

    Bump version

--- project.clj
+++ project.clj
@@ -18,7 +18,7 @@
   ['clojure.tools.logging.impl
    'ventas.core])
 
-(defproject ventas "0.0.5"
+(defproject ventas "0.0.6-SNAPSHOT"
   :description "The Ventas eCommerce platform"
 
   :url "https://github.com/JoelSanchez/ventas"
