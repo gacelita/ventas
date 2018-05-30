2b2a1c02959d8250f92754167aa5fe6daaeef418
Joel Sánchez <webmaster@kazer.es>
2018-05-30 22:11:38 +0200

    Add "core" namespaces for email templates, entities and plugins

---
+++ src/clj/ventas/email/templates/core.clj
@@ -0,0 +1,5 @@
+(ns ventas.email.templates.core
+  (:require
+   [ventas.email.templates.order-status-changed]
+   [ventas.email.templates.password-forgotten]
+   [ventas.email.templates.user-registered]))
