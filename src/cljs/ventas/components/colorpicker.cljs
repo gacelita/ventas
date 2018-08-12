9fccb4677ade07a9177b4b0a2dbc54f95516e537
Joel Sánchez <webmaster@kazer.es>
2018-08-12 15:49:36 +0200

    Add colorpicker-input

--- src/cljs/ventas/components/colorpicker.cljs
+++ src/cljs/ventas/components/colorpicker.cljs
@@ -11,7 +11,13 @@
     (->> {:disableAlpha false
           :color value
           :onChangeComplete (fn [color _]
-                              (rf/dispatch (conj on-change (js->clj color :keywordize-keys true))))}
+                              (rf/dispatch (conj on-change (:hex (js->clj color :keywordize-keys true)))))}
          (remove (fn [[k v]] (nil? v)))
          (into {})
          clj->js))])
+
+(defn colorpicker-input [{:keys [on-change value]}]
+  [:div.colorpicker-input
+   [:input {:value (or value "")}]
+   [colorpicker {:on-change on-change
+                 :value value}]])
