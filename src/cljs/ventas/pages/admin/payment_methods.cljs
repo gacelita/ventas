1e9ea854e5148b66fa62c7c840b23d1efc22feec
Joel Sánchez <webmaster@kazer.es>
2018-09-22 17:15:35 +0200

    Add payment methods generic page

--- src/cljs/ventas/pages/admin/payment_methods.cljs
+++ src/cljs/ventas/pages/admin/payment_methods.cljs
@@ -2,12 +2,20 @@
   (:require
    [ventas.i18n :refer [i18n]]
    [ventas.pages.admin.skeleton :as admin.skeleton]
-   [ventas.routes :as routes]))
+   [ventas.pages.admin.configuration :as admin.configuration]
+   [ventas.routes :as routes]
+   [re-frame.core :as rf]))
+
+(defn get-items []
+  (->> @(rf/subscribe [::admin.skeleton/menu-items])
+       (filter #(= (:route %) :admin.payment-methods))
+       first
+       :children))
 
 (defn- page []
   [admin.skeleton/skeleton
    [:div.admin__default-content.admin-payment-methods__page
-    [:p "Nothing here!"]]])
+    [admin.configuration/menu (get-items)]]])
 
 (routes/define-route!
   :admin.payment-methods
