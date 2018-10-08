12a3a332d4379919c229664c7fa8b2ea3dd6eca1
Joel Sánchez <webmaster@kazer.es>
2018-10-08 11:02:12 +0200

    Don't throw if a spec fails, just log an error

--- src/cljs/ventas/utils.cljs
+++ src/cljs/ventas/utils.cljs
@@ -4,7 +4,7 @@
    [cljs.spec.alpha :as spec]
    [expound.alpha :as expound]
    [ventas.utils.logging :refer [debug error info trace warn]]
-   [clojure.string :as str])
+   [ventas.utils.logging :as log])
   (:require-macros
    [ventas.utils]))
 
@@ -41,12 +41,10 @@
   ([sep coll]
    (drop 1 (interleave (repeatedly sep) coll))))
 
-(defn check [& args]
-  "([spec x] [spec x form])
-     Returns true when x is valid for spec. Throws an Error if validation fails."
-  (if (apply spec/valid? args)
-    true
-    (throw (js/Error. (with-out-str (apply expound/expound args))))))
+(defn check [spec x]
+  (when-not (spec/valid? spec x)
+    (log/error (with-out-str (expound/expound spec x))))
+  true)
 
 (defn debounce
   "Returns a function that will call f only after threshold has passed without new calls
