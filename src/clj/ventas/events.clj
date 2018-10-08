c3e57bc69af9a331a5d33975dce5f17839a598a8
Joel Sánchez <webmaster@kazer.es>
2018-10-08 10:54:02 +0200

    Use a sliding buffer in the events ns

--- src/clj/ventas/events.clj
+++ src/clj/ventas/events.clj
@@ -1,11 +1,11 @@
 (ns ventas.events
   (:require
-   [clojure.core.async :as core.async :refer [<! >! chan go-loop]]))
+   [clojure.core.async :as core.async :refer [<! >! chan go-loop sliding-buffer]]))
 
 (defonce events (atom {}))
 
 (defn register-event! [evt-name]
-  (let [ch (chan)
+  (let [ch (chan (sliding-buffer 10))
         evt-data {:chan ch :mult (core.async/mult ch)}]
     (swap! events assoc evt-name evt-data)
     evt-data))
@@ -20,5 +20,7 @@
     (get data :chan)))
 
 (defn sub [evt-name]
-  (let [data (event evt-name)]
-    (core.async/tap (get data :mult) (chan))))
+  (let [data (event evt-name)
+        ch (chan)]
+    (core.async/tap (get data :mult) ch)
+    ch))
