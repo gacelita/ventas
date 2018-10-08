1049432f62158032eb7f60f12d4302972d07a084
Joel Sánchez <webmaster@kazer.es>
2018-10-08 11:13:16 +0200

    Add :target to components.menu

--- src/cljs/ventas/components/menu.cljs
+++ src/cljs/ventas/components/menu.cljs
@@ -1,23 +1,44 @@
 (ns ventas.components.menu
   (:require
-   [ventas.components.base :as base]))
+   [ventas.components.base :as base]
+   [cljs.spec.alpha :as spec]
+   [ventas.utils :as utils]))
+
+(spec/def ::href string?)
+(spec/def ::text string?)
+(spec/def ::target string?)
+(spec/def ::children
+  (spec/coll-of ::item))
+
+(spec/def ::item
+  (spec/keys :opt-un [::href
+                      ::text
+                      ::children
+                      ::target]))
+
+(spec/def ::items
+  (spec/coll-of ::item))
 
 (defn menu-children [children]
   [:div.menu__children
-   (for [{:keys [id text href] :as child} children]
+   (for [{:keys [id text href target] :as child} children]
      ^{:key (or id (hash child))}
-     [:a.menu__child {:href href} text])])
+     [:a.menu__child {:href href
+                      :target target} text])])
 
-(defn menu-item [{:keys [href text children]} current?]
+(defn menu-item [{:keys [href target text children] :as a} current?]
   [:li.menu__item
    {:class (when current? "menu__item--active")}
-   [:a {:href href} text]
+   [:a {:href href
+        :target target}
+    text]
    (when children
      [:div.menu__overlay
       [base/container
        [menu-children children]]])])
 
 (defn menu [{:keys [current-fn items]}]
+  {:pre [(utils/check ::items items)]}
   [:div.menu
    [base/container
     [:ul.menu__items
