95e58bb3a15418512d02c7af908c4e1fd24bce4e
Joel Sánchez <webmaster@kazer.es>
2018-11-13 10:01:42 +0100

    Add pure-reaction

--- src/cljs/ventas/utils/re_frame.cljs
+++ src/cljs/ventas/utils/re_frame.cljs
@@ -1,5 +1,10 @@
 (ns ventas.utils.re-frame)
 
+(defn pure-reaction [db sub-and-params]
+  ((re-frame.registrar/get-handler :sub (first sub-and-params))
+   db
+   sub-and-params))
+
 (defn pure-subscribe
   "Use subscriptions inside event handlers in a pure way."
   [db sub-and-params]
